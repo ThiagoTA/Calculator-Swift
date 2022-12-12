2ae3af300963e4a0a151634d36a9398b25a538fd
ThiagoTA <thiago.teixeirapf@gmail.com>
2022-12-12 11:22:28 -0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Thiago Teixeira on 12/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
